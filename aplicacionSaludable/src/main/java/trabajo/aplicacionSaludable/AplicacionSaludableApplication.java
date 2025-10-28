6a12af43afa57e5b90f644cd6ff216f0d6bc2801
alex <aag340@alumnos.unican.es>
2025-10-28 19:49:47 +0100

    Proyecto spring creado

---
+++ aplicacionSaludable/src/main/java/trabajo/aplicacionSaludable/AplicacionSaludableApplication.java
@@ -0,0 +1,16 @@
+package trabajo.aplicacionSaludable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacionSaludableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacionSaludableApplication.class, args);
+
+		System.out.println("Iniciando Spring Boot Application");
+
+	}
+
+}
---
+++ aplicacionSaludable/src/test/java/trabajo/aplicacionSaludable/AplicacionSaludableApplicationTests.java
@@ -0,0 +1,13 @@
+package trabajo.aplicacionSaludable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacionSaludableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
